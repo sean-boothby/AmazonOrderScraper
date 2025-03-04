0f79d2f72094be7689283799597e84c7d6248b60
Sean Boothby <sean.boothby@gmail.com>
2025-03-04 08:00:44 -0600

    Cleaning up

--- orderFetcher.py
+++ orderFetcher.py
@@ -60,10 +60,6 @@ def makerequest(orderindex):
     }
     response = requests.get(url, headers=headers)
     return response
-
-def savetxt(text):
-    with open("orderdeets.txt", "a", encoding="utf-8") as file:
-        file.write(text + "\n")
         
         
 def test_moreorders(r):
@@ -130,7 +126,7 @@ def get_price(oid):
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "accept-encoding": "gzip, deflate, br, zstd",
         "accept-language": "en-US,en;q=0.9",
-        "cookie": cookie,
+        "cookie": cookie_price,
         #"cookie": "i18n-prefs=USD; x-amz-captcha-1=1730582410553894; x-amz-captcha-2=TYuB1uSmiBmpLooxNYGyAg==; ubid-main=132-8193239-7735707; lc-main=en_US; s_nr=1736718953661-New; s_vnum=2168718953662&vn=1; s_dslv=1736718953662; csd-key=eyJ3YXNtVGVzdGVkIjp0cnVlLCJ3YXNtQ29tcGF0aWJsZSI6dHJ1ZSwid2ViQ3J5cHRvVGVzdGVkIjpmYWxzZSwidiI6MSwia2lkIjoiMzlkMTE2Iiwia2V5IjoiQ1d4bFdBRW5QNE9qY3N6Rk5ucytUVDdnMktlM3RpaUFlSVFsVmRmU21vUi91bmhwWjdtQTVnc3NBOGExSGtoQUtybkdkL1F6NEhSTHIyUnYxamZNQWJZcDJYaUVCV0pGcEFhZEs5U2RtOEUxeVR0djcvUFJISjFHTnBuRHpxNFRKRFRZMjdmdkRmWUs4cjByWlRvRktzd1JEM0FDWHNIZzR1TWZZeno3MmM0dU41QjZRc3NlMTFrOGdUMk9kSkw3NUdyVTI4VC80MmxTU05CeUxwVzE5TVBrT2h3eE5ZWGRPNXJhWnhraC82VkpUcTgvZkdlVGJjZmRaeDZNcWI2U2JnV1hmWWVnQTVVK1FIdlE1Z1k3OGhsdmlvTzk2VWRKZnJ5dS9iMEkrK2ZzVzdTTnA1ZGxYdnBtSE1MSkJyN2RRajdpOHJ1Q1Y3dWZ2eEJIMHVSYk1BPT0ifQ==; skin=noskin; session-id=137-1415228-6188013; JSESSIONID=B5CA31A65363397BF7EE20A06B6C3BD5; at-main=Atza|IwEBIJKrSrhkr0mEg0F9xl3k6CLUyze8mz0JaKmhQn8DRn5N4UyzyHmycAASCs0WOu2QwDtXjPP9J1Cccr4ppc4Ag5l65vrTWvX4MdmTqQ1KPwE5dhBTvh2G8ZHGmmm5gJkg933n_HwqAd45-FPJpwGrUSjcmKatuJocPiC3MXlFihF2QqCfdVB_vdMbs5uzmDvhPMF27RRmcqJt7p2-dAj5XE9uffrxq3HGVIqmIHRp0pbMcg; sess-at-main=\"Zr8Xm0+lru4q3wkDjkiZmj1z+Ry18mIUpYm+tg2pJV8=\"; sst-main=Sst1|PQGSH2YnGD-xyE3TReRuXXz7CdF_SKT8EXRrjT2CJW8a9Ajp6HIQ_Xre2fKwBJtGD_CL6w7cCSSykG66BPCvNGrWLtltjmsxx4izDlp-mDP7oN-l54b0bEf3VNfkKws4Dbnkkxcnx6LSZX4BVIqMyEvS2TDBU2SJNgElsPoDdVZjNz_uSKAhv1hCij1LxQz8Wg1f6tXKYVa9nVTkKOu43LbVa3LFQnDZuc3nNVxLB0_zgx4a7mLv-3ODeB7fxq0vfSGbvSDCrEhIZvftmwlE1Hn37W1KGT_VWJq3Diti8DSMVrI; session-id-time=2082787201l; session-token=oOeRidIzcFDDIZN5AAVN/BB9opZ9TSvSzCMj2A9Qsy/ER4bE5g2nbKwrf0z67UfpGYJP/gTL+FVoz+4gZkCbaSphDmaBpMY2zO+1/LjzLxxrstgM5jjyQjkLyQPyZEm+iEne3F0sSAnZWNxKbu0eynURCHqFkLAiNKga6nAXaucJOI5FsIjf60cheC70JQjOaS8wNH6jr9wjJFHEE2HQDdMvSxjqlfeRbISqcCX5QJeKOMPZhDQJg4iDcgyC/larh0UX3XfR4oFT998M5rCXgKBG4ealcJ1ZEQBvN/Y+y4lWn2qc9VBuq2onh78zvYSxd4rQBadpJ4JEtlABEHar+iyn7aAFW5P6UX3q0BnwEc/1xZY9eDaBlt1kguwf91oS; x-main=\"q6sLk6ZrUIftC2KrHLWlCr7LwrNEhCVtS3OxpI@0i8t7I2FeJrl8QMEs?Z5UJT6B\"; csm-hit=FJ2SB9EVWGQ96M182PA1+s-AHRPCG69XMEEQGC62N5X|1740929325511",
         "device-memory": "8",
         "downlink": "1.8",
@@ -166,5 +162,3 @@ while ordertest:
     ordertest = test_moreorders(r)
     orderindex += 10
     parsedata(r)
-#     # print(orderindex)
-#     time.sleep(0.5)
